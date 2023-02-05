b64f747477840a05d45db447af4872ff759cf1a0
Roberto Mora <robertomorado.coro2@gmail.com>
2023-02-05 10:19:04 -0400

    Cambios

--- scripts/script-product.js
+++ scripts/script-product.js
@@ -161,7 +161,7 @@ cartButtons.forEach((e) => {
 		Push.create("Hello world!",{
 			body: "This is example of Push.js Tutorial",
 			icon: 'media/Logo1.png',
-			timeout: 2000,
+			timeout: 20000,
 			onClick: function () {
 				window.focus();
 				this.close();
